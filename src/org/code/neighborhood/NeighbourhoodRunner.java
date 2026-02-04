8156761d75e64970b9a2cfcd53a24098da31facf
Ritchiel Reza <ritchielrez@gmail.com>
2026-02-04 14:47:17 -0500

    refactor(NeighbourhoodRunner): `gridCount` is a better var name than `gridSize`

--- src/org/code/neighborhood/NeighbourhoodRunner.java
+++ src/org/code/neighborhood/NeighbourhoodRunner.java
@@ -2,8 +2,9 @@ import org.code.neighborhood.*;
 
 public class NeighborhoodRunner {
   public static void main(String[] args) {
-    final int gridSize = 16;
-    AsepriteLogo logoPainter = new AsepriteLogo(gridSize, gridSize);
+    // Number of grids in a row or column
+    final int gridCount = 16;
+    AsepriteLogo logoPainter = new AsepriteLogo(gridCount, gridCount);
     logoPainter.setPaint(Integer.MAX_VALUE);
     logoPainter.paintLogo();
   }
