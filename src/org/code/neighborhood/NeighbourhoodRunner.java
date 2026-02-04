e61c70034384b568dcc59daac62a40df27d94761
Ritchiel Reza <ritchielrez@gmail.com>
2026-02-04 14:15:05 -0500

    fix(NeighbourhoodRunner): have `Integer.MAX_VALUE` number of buckets so we never run out of them.

--- src/org/code/neighborhood/NeighbourhoodRunner.java
+++ src/org/code/neighborhood/NeighbourhoodRunner.java
@@ -4,7 +4,7 @@ public class NeighborhoodRunner {
   public static void main(String[] args) {
     final int gridSize = 16;
     AsepriteLogo logoPainter = new AsepriteLogo(gridSize, gridSize);
-    logoPainter.setPaint(gridSize * gridSize);
+    logoPainter.setPaint(Integer.MAX_VALUE);
     logoPainter.paintLogo();
   }
 }
