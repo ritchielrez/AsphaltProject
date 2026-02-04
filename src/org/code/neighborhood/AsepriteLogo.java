cebedaba6e18158c3c406e832c6a6d2cac79539a
Ritchiel Reza <ritchielrez@gmail.com>
2026-02-04 14:16:51 -0500

    feat(AsepriteLogo): finish the full logo

--- src/org/code/neighborhood/AsepriteLogo.java
+++ src/org/code/neighborhood/AsepriteLogo.java
@@ -11,45 +11,76 @@ public class AsepriteLogo extends PainterPlus {
     super(gridRows, gridCols);
   }
 
+  // Paints a white line except the first and the last pixels use the color `fg0`.
+  void paintWhiteLine(int y) {
+    paintGrid(0, y, fg0);
+    paintLineX(1, gridCols - 1, bg);
+    paintGrid(gridCols - 1, y, fg0);
+  }
+  
   void paintLogo() {
     // Draws the logo row by row. The code inside each curly braces below represent each row of the logo. 
     int y = 0;
     { 
       paintGrid(0, y, bg);
       paintGrid(1, y, fg1);
-      paintLineX(2, gridRows - 2, fg0);
-      paintGrid(gridRows - 2, y, fg1);
-      paintGrid(gridRows - 1, y, bg);
+      paintLineX(2, gridCols - 2, fg0);
+      paintGrid(gridCols - 2, y, fg1);
+      paintGrid(gridCols - 1, y, bg);
     } 
     y++;
     {
       paintGrid(0, y, fg1);
       paintGrid(1, y, fg0);
-      paintLineX(2, gridRows - 2, bg);
-      paintGrid(gridRows - 2, y, fg0);
-      paintGrid(gridRows - 1, y, fg1);
+      paintLineX(2, gridCols - 2, bg);
+      paintGrid(gridCols - 2, y, fg0);
+      paintGrid(gridCols - 1, y, fg1);
     }
-    y++;
-    {
-      paintGrid(0, y, fg0);
-      paintLineX(1, gridRows - 1, bg);
-      paintGrid(gridRows - 1, y, fg0);
+    for(int i = 1; i <= 2; i++) {
+      paintWhiteLine(++y);  
     }
-    y++;
-    {
-      paintGrid(0, y, fg0);
-      paintLineX(1, gridRows - 1, bg);
-      paintGrid(gridRows - 1, y, fg0);
-    }
-    y++;
-    {
+    for (int i = 1; i <= 5; i++) {
+      y++;
       paintGrid(0, y, fg0);
       paintLineX(1, 5, bg);
       paintGrid(5, y, fg0);
-      paintLineX(6, 11, bg);
-      paintGrid(11, y, fg0);
-      paintLineX(6, 11, bg);
-      paintGrid(gridRows-  1, y, fg0);
+      paintLineX(6, 10, bg);
+      paintGrid(10, y, fg0);
+      paintLineX(11, gridCols - 1, bg);
+      paintGrid(gridCols - 1, y, fg0);
     }
+    for(int i = 1; i <= 3; i++) {
+      paintWhiteLine(++y);  
+    }
+    y++;
+    {
+      paintGrid(0, y, fg0);
+      paintGrid(1, y, blue);
+      paintLineX(2, gridCols - 2, bg);
+      paintGrid(gridCols - 2, y, blue);
+      paintGrid(gridCols - 1, y, fg0);
+    }
+    y++;
+    {
+      paintGrid(0, y, fg0);
+      paintLineX(1, gridCols - 1, blue);
+      paintGrid(gridCols - 1, y, fg0); 
+    }
+    y++;
+    { 
+      paintGrid(0, y, fg1);
+      paintGrid(1, y, fg0);
+      paintLineX(2, gridCols - 2, blue);
+      paintGrid(gridCols - 2, y, fg0);
+      paintGrid(gridCols - 1, y, fg1);
+    }
+    y++;
+    { 
+      paintGrid(0, y, bg);
+      paintGrid(1, y, fg1);
+      paintLineX(2, gridCols - 2, fg0);
+      paintGrid(gridCols - 2, y, fg1);
+      paintGrid(gridCols - 1, y, bg);
+    } 
   }
 }
